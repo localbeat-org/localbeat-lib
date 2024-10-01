166de90b7212d70afd1e00e86c898ed4d0452f19
etewa <jcamire@gmail.com>
2024-10-01 10:13:53 -0600

    Added helper methods to bounding box

--- src/main/java/org/localbeat/api/geo/BoundingBox.java
+++ src/main/java/org/localbeat/api/geo/BoundingBox.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 /****************************************************************************
  * <b>Title:</b> BoundingBox.java <br>
  * <b>Project:</b> Entertainment <br>
- * <b>Description:</b> --- Change Me --- <br>
+ * <b>Description:</b> A bean to hold the max and min coordinates of the bounding box <br>
  * <b>Copyright:</b> Copyright (c) 2023 <br>
  * <b>Company:</b> Local Beats
  * 
@@ -35,4 +35,44 @@ public class BoundingBox {
 	 */
 	@JsonProperty(value="northeast")
 	private Coordinate maxPoint;
+	
+	/**
+	 * Returns the smallest value of the latitudes.  This is due to the fact that
+	 * there can be negative numbers
+	 * @return
+	 */
+	public double getMinimumLatitudeValue() {
+		
+		return minPoint.getLatitude() < maxPoint.getLatitude() ? minPoint.getLatitude() : maxPoint.getLatitude();
+	}
+	
+	/**
+	 * Returns the smallest value of the latitudes.  This is due to the fact that
+	 * there can be negative numbers
+	 * @return
+	 */
+	public double getMinimumLongitudeValue() {
+		
+		return minPoint.getLongitude() < maxPoint.getLongitude() ? minPoint.getLongitude() : maxPoint.getLongitude();
+	}
+	
+	/**
+	 * Returns the smallest value of the latitudes.  This is due to the fact that
+	 * there can be negative numbers
+	 * @return
+	 */
+	public double getMaximumLatitudeValue() {
+		
+		return minPoint.getLatitude() > maxPoint.getLatitude() ? minPoint.getLatitude() : maxPoint.getLatitude();
+	}
+	
+	/**
+	 * Returns the smallest value of the latitudes.  This is due to the fact that
+	 * there can be negative numbers
+	 * @return
+	 */
+	public double getMaximumLongitudeValue() {
+		
+		return minPoint.getLongitude() > maxPoint.getLongitude() ? minPoint.getLongitude() : maxPoint.getLongitude();
+	}
 }
